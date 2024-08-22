f166eaa4c238fcc5904175049b6f604e92072d09
Chaitany233patil <patilchaitany529@gmail.com>
2024-08-22 21:17:19 +0530

    added KeyPress Event

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 let btns = document.querySelectorAll("button");
 let input = document.querySelector("input");
 
+function keyPress(value) {
+  input.value += value;
+}
+
+function calculate() {
+  input.value = eval(input.value);
+}
+
+document.addEventListener("keypress", (event) => {
+  if (event.code == `Digit${event.key}`) {
+    keyPress(event.key);
+  } else if (event.code == `Digit8`) {
+    keyPress(event.key);
+  } else if (event.code == `Minus`) {
+    keyPress(event.key);
+  } else if (event.code == `Equal`) {
+    if (event.key == "+") keyPress(event.key);
+  } else if (event.code == "Slash") {
+    keyPress(event.key);
+  } else if (event.code == "Enter") {
+    calculate();
+  } else {
+    console.log(event);
+  }
+});
+
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
     if (btn.innerHTML == "=") {
       input.value = input.value.replace("x", "*");
-      input.value = eval(input.value);
+      calculate();
     } else if (btn.innerHTML == "Bs") {
       let newStr = input.value.slice(0, -1);
       input.value = newStr;
     } else if (btn.innerHTML == "C") {
-      input.value = 0;
+      input.value = null;
     } else {
       input.value += btn.innerHTML;
     }
